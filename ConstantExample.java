7b04a4e6c44d21d23cd66321b871bac356787381
Mrsaugatshah <saugats082@gmail.com>
2025-11-21 21:54:52 +0545

    initial commit

---
+++ ConstantExample.java
@@ -0,0 +1,8 @@
+class ConstantExample {
+
+    public static void main(String[] args) {
+        final int a = 10;
+        System.out.println("a = " + a);
+
+    }
+}
---
+++ TypesofVariable.java
@@ -0,0 +1,14 @@
+
+class TypesofVariable
+{
+    int a = 5; //instance variable
+    static int b = 10; // static variable
+    void method()
+    {
+        int c = 20; //local variable
+    }
+    public static void main(String[] args)
+    {
+        System.out.println("b = "+b);
+    }
+}
